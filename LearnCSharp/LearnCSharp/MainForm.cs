9b4c796653a82329654f4b29a5e2f74af3537a91
Ioana Moldovan <IoanaAlexandra.Moldovan@harman.com>
2019-06-26 11:10:46 +0300

    Delegate examle added

--- LearnCSharp/LearnCSharp/MainForm.cs
+++ LearnCSharp/LearnCSharp/MainForm.cs
@@ -5,6 +5,21 @@ namespace LearnCSharp
 {
     public partial class MainForm : Form
     {
+        public delegate void myDelegate();
+
+        public static int current_value = 0;
+        myDelegate mydele;
+
+        public void showValue()
+        {
+            my_textbox.AppendText("My Value is: " + current_value + "\r\n");
+        }
+
+        public void incrementValue()
+        {
+            my_textbox.AppendText("My Incremented Value is: " + ++current_value + "\r\n");
+        }
+
         public MainForm()
         {
             InitializeComponent();
@@ -13,11 +28,14 @@ namespace LearnCSharp
         private void MainForm_Load(object sender, EventArgs e)
         {
             my_textbox.AppendText("Hello\r\n");
+
+            mydele = new myDelegate(showValue);
+            mydele += incrementValue;
         }
 
         private void mytimer_Tick(object sender, EventArgs e)
         {
-            my_textbox.AppendText("New line\r\n");
+            mydele.Invoke();
         }
 
         private void start_btn_Click(object sender, EventArgs e)
