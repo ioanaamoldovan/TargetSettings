059be6d76f6feb1afd65be75634664b6faa1fcd1
Ioana Moldovan <IoanaAlexandra.Moldovan@harman.com>
2019-06-26 10:12:59 +0300

    Working with timers

---
+++ LearnCSharp/LearnCSharp/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace LearnCSharp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.mytimer = new System.Windows.Forms.Timer(this.components);
+            this.my_textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(49, 203);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(94, 46);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(194, 203);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(94, 46);
+            this.stop_btn.TabIndex = 1;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // mytimer
+            // 
+            this.mytimer.Interval = 1000;
+            this.mytimer.Tick += new System.EventHandler(this.mytimer_Tick);
+            // 
+            // my_textbox
+            // 
+            this.my_textbox.Location = new System.Drawing.Point(49, 25);
+            this.my_textbox.Multiline = true;
+            this.my_textbox.Name = "my_textbox";
+            this.my_textbox.Size = new System.Drawing.Size(239, 138);
+            this.my_textbox.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(337, 278);
+            this.Controls.Add(this.my_textbox);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Timer mytimer;
+        private System.Windows.Forms.TextBox my_textbox;
+    }
+}
+
---
+++ LearnCSharp/LearnCSharp/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LearnCSharp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            my_textbox.AppendText("Hello\r\n");
+        }
+
+        private void mytimer_Tick(object sender, EventArgs e)
+        {
+            my_textbox.AppendText("New line\r\n");
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            mytimer.Enabled = true;
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            mytimer.Enabled = false;
+        }
+    }
+}
