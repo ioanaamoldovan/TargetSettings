b6d46ff52213ac863a6d2b16db395263eecc83e5
Ioana Moldovan <IoanaAlexandra.Moldovan@harman.com>
2019-07-01 16:27:46 +0300

    Static Library https://docs.microsoft.com/en-us/cpp/build/walkthrough-creating-and-using-a-static-library-cpp?view=vs-2019

---
+++ MathFuncsLib/MathFuncsLib/MathFuncsLib.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace MathFuncs
+{
+	class MyMathFuncs
+	{
+	public:
+		static double Add(double a, double b);
+		static double Subtract(double a, double b);
+		static double Multiply(double a, double b);
+		static double Divide(double a, double b);
+	};
+}
